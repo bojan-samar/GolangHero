b688f0d8d1f738b7bd29eb5022074a87d271ee0d
Bojan Samardzija <bojan.samar@gmail.com>
2022-11-13 17:01:15 -0800

    Update

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -16,11 +16,11 @@ defineProps({
 
 onMounted(() => {
     //Load Google Analytics
-    // let googleAnalyticsTag = usePage().props.value.tracking.googleAnalytics;
-    // if (googleAnalyticsTag){
-    //     gtag('js', new Date());
-    //     gtag('config', `${googleAnalyticsTag}`);
-    // }
+    let googleAnalyticsTag = usePage().props.value.tracking.googleAnalytics;
+    if (googleAnalyticsTag){
+        gtag('js', new Date());
+        gtag('config', `${googleAnalyticsTag}`);
+    }
 });
 
 const showingNavigationDropdown = ref(false);
