5c14e1501ba3e6103493855dd93e9a8e59396c0b
Bojan Samardzija <bojan.samar@gmail.com>
2022-11-19 07:40:29 -0800

    Update

--- app/Models/Account/WorkerSkill.php
+++ app/Models/Account/WorkerSkill.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Account;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -16,5 +17,12 @@ class WorkerSkill extends Model
         static::creating(function ($model) {
             $model->uuid = Str::uuid();
         });
+    }
+
+    protected function firstName(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => Str::title($value),
+        );
     }
 }
