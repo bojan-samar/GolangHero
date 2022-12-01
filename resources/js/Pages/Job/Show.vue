6b7666c41573809309a2f14f562bdc797580da9e
Bojan Samardzija <bojan.samar@gmail.com>
2022-11-30 19:43:44 -0800

    Update

--- resources/js/Pages/Job/Show.vue
+++ resources/js/Pages/Job/Show.vue
@@ -60,7 +60,7 @@ onMounted(() => {
             <meta name="twitter:card" content="summary">
             <meta name="twitter:title" :content="job.data.title">
             <meta name="twitter:description" :content="job.data.title">
-            <meta property="og:image" content="" />
+            <meta property="og:image" :content="job.data.company.photoUrl" />
         </Head>
 
         <component :is="'style'">
