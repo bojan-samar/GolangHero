f61b3b86236f355cf745c8d00198f86777c4939b
Bojan Samardzija <bojan.samar@gmail.com>
2022-12-03 15:40:07 -0800

    Update

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,6 +1,10 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
+        <loc>{{ route('sitemap.main') }}</loc>
+    </sitemap>
+
+    <sitemap>
         <loc>{{ route('sitemap.jobs') }}</loc>
     </sitemap>
 </sitemapindex>
