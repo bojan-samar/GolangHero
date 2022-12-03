8b7f36d4fe61b57fd4f6d7d214e5bba3efc81f0e
Bojan Samardzija <bojan.samar@gmail.com>
2022-12-03 12:47:53 -0800

    Update

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,6 +1,5 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
     <url>
         <loc>https://golanghero.com/</loc>
         <lastmod>2022-11-09T05:51:11+00:00</lastmod>
@@ -12,8 +11,11 @@
         <lastmod>2022-11-09T05:51:11+00:00</lastmod>
         <priority>0.90</priority>
     </url>
+</urlset>
 
+
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
         <loc>{{ route('sitemap.jobs') }}</loc>
     </sitemap>
-</urlset>
+</sitemapindex>
